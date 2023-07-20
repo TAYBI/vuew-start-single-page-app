beba103c229a07744dcdda9816ae08544324e676
Bilal <taybib4@gmail.com>
2023-07-20 13:15:23 +0100

    i bootstrap

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,13 @@
 <template>
     <nav class="navbar d-flex flex-row" :class="navBarClasses">
-        <div class="container-fluid">
+        <div class="container container-fluid">
             <span class="navbar-brand mb-0 h1">Navbar</span>
             <div class="navbar-nav d-flex flex-row justify-content-sm-between">
                 <a v-for="(page, index) in pages" class="p-2 nav-link" :class="{ active: activePage == index }"
                     :href="page.link.url" :key="index" @click.prevent="navLinkClick(index)">
                     {{ page.link.text }}
                 </a>
-                <button class="btn btn-primary" type="submit" @click="useDarkMode = !useDarkMode">
+                <button class="btn btn-primary ml-2" type="submit" @click="useDarkMode = !useDarkMode">
                     Mode</button>
             </div>
         </div>
